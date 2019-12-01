37c1aa3c11eff37ed541ab53218025aa728828b9
a-kosanovic <kiselo.slatkok@gmail.com>
2019-12-01 02:16:25 +0100

    bugfix: toggle bootstrap dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,34 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef, Renderer2, OnDestroy } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
   exportAs:'appDropdown'
 })
 
-export class DropdownDirective {
-  @HostBinding('class.show') isOpen = false;
+export class DropdownDirective implements OnDestroy{
+  // @HostBinding() isOpen = false;
+  nativeEl = null;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = true;
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  @HostListener('click') toggleDropdown() {
+    this.nativeEl =  this.elementRef.nativeElement;
+    const dropdownMenu = this.nativeEl.querySelector('.dropdown-menu');
+
+    if (this.nativeEl.classList.contains('show')) {
+      this.renderer.removeClass(this.nativeEl, 'show');
+      this.renderer.removeClass(dropdownMenu, 'show');
+      return;
+    }
+    else {
+      this.renderer.addClass(this.nativeEl, 'show');
+      this.renderer.addClass(dropdownMenu, 'show');
+    }
+  }
+
+  ngOnDestroy() {
+    this.nativeEl = null;
   }
 }
