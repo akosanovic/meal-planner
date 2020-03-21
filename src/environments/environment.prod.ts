66381fd7825e9796ed1ec385b2e0cd6a01a8d12e
a-kosanovic <kiselo.slatko@gmail.com>
2020-03-21 03:38:09 +0100

    Depolyed app to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDdC8rIsp6hfxZlr0_2H3qV24w3dbmOrUc',
+    authDomain: 'chronoplanner.firebaseapp.com',
+    databaseURL: 'https://chronoplanner.firebaseio.com',
+    projectId: 'chronoplanner',
+    storageBucket: 'chronoplanner.appspot.com',
+    messagingSenderId: '856022917091',
+    appId: '1:856022917091:web:813f6fc152d83e6ef2cad6',
+    measurementId: 'G-XSFHCHWHTE'
+  }};
